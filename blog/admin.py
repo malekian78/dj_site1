7f95e92429b802006d14749e5e4c29534809c4ec
MohamadHosein <mh.malekian78@gmail.com>
2024-02-09 23:23:02 +0330

    اضافه کردن نویسنده از طریق یوزر خود جنگو

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,11 @@ from blog.models import Post
 # Register your models here.
 
 class postAdmin(admin.ModelAdmin): #? https://docs.djangoproject.com/en/4.2/ref/contrib/admin/
-    list_display = ('pk',"title", 'status', 'published_date', 'created_date', 'updated_date')
+    date_hierarchy = 'created_date'
+    list_display = ('pk',"title",'author', 'status', 'published_date', 'updated_date')
+    empty_value_display = "_خالی_"
+    list_filter = ('status','author')
+    search_fields = ['title', 'content']
+    
 
 admin.site.register(Post, postAdmin)
