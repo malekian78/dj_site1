91b3f2ae1f1ee546e9a412fa6122e035193e1402
MohamadHosein <mh.malekian78@gmail.com>
2024-02-11 23:20:50 +0330

    using **kwargs (key , value) in view ✅

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
-def blog_view(request, cat_name=None, author_username = None):
+# def blog_view(request, cat_name=None, author_username = None):
+def blog_view(request, **kwargs):
     posts = Post.objects.filter(status=True)
-    if cat_name:
-        posts = posts.filter(category__name=cat_name)
-    if author_username:
-        posts = posts.filter(author__username = author_username)
+    # if cat_name:
+    #     posts = posts.filter(category__name=cat_name)
+    # if author_username:
+    #     posts = posts.filter(author__username = author_username)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username = kwargs['author_username'])
     context = {'posts':posts, "test":30}
     return render(request, 'blog/blog-home.html', context)
 
