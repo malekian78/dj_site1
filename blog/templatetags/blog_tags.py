70eb5ab87473ba5bc191f041e7e49ceb6e6b5c6a
MohamadHosein <mh.malekian78@gmail.com>
2024-02-11 22:28:32 +0330

    نمایش قسمت post categories و تعداد پست مربوط به هرکدام

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post,Category
 register = template.Library()
 
 
@@ -21,7 +21,7 @@ def allpost():
 def myFilter(value,num=30):
     return value[:num]
 
-@register.inclusion_tag('last3posts.html')
+@register.inclusion_tag('last3postsTEST.html')
 def test_last3posts():
     posts = Post.objects.filter(status=1).order_by('published_date')[:3]
     return {'posts': posts}
@@ -31,3 +31,14 @@ def test_last3posts():
 def last3posts():
     posts = Post.objects.filter(status=1).order_by('published_date')[:3]
     return {'posts': posts}
+#!________________________________________________
+#! مقدار دسته بندی ها و تعداد پست هر دسته بندی
+@register.inclusion_tag('blog/post-categories.html')
+def post_categories():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    print(type(posts))
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+    return {'categories':cat_dict}
