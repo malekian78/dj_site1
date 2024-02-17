6b856aa567d8c8ff2d9addd46419bdec135787a1
MohamadHosein <mh.malekian78@gmail.com>
2024-02-17 17:05:13 +0330

    ✔ humanize, ✔ django extensions, ✔Django Sites Framework

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,13 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    'django.contrib.humanize', #https://docs.djangoproject.com/en/4.2/ref/contrib/humanize/
+    'django_extensions', #https://github.com/django-extensions/django-extensions?tab=readme-ov-file
+    'django.contrib.sites', #https://docs.djangoproject.com/en/4.2/ref/contrib/sites/
+    # 'django.contrib.sitemaps', #https://docs.djangoproject.com/en/4.2/ref/contrib/sitemaps/
     
     # 'blog.apps.BlogConfig',
     'blog',
     'website',
     'jalali_date',#!https://pypi.org/project/django-jalali-date/
 ]
+
+#! و آیدی شماره ۱ مراجعه میکند django_site برای اینکه بفهمه آدرس سایت من چیه؟....خب مستقیم به پایگاه داده 
+SITE_ID = 1 #https://docs.djangoproject.com/en/4.2/ref/contrib/sites/
 
 #!https://pypi.org/project/django-jalali-date/
 #! default settings (optional)
